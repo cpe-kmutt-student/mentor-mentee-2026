5490d2b34f535838e88c0e52b5a6c7e712259579
ImJustNon <Im_Just_Non@hotmail.com>
2026-06-24 01:12:42 +0700

    feat: allow database client build

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,19 +1,21 @@
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg'; // Import the Pool
-import { PrismaClient } from "../generated/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from "@repo/config";
+import { Pool } from "pg";
+import { PrismaClient } from "../generated/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const pool = new Pool({
-  connectionString: config.backend.database_url
+	connectionString: config.backend.database_url,
 });
 export const adapter = new PrismaPg(pool);
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-    adapter,
-    log: ['query', 'info', 'warn', 'error'],
-});
+export const prisma =
+	globalForPrisma.prisma ||
+	new PrismaClient({
+		adapter,
+		log: ["query", "info", "warn", "error"],
+	});
 
 if (config.node_env !== "production") globalForPrisma.prisma = prisma;
 
