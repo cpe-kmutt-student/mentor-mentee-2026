157fe6967c6b9e6a977af42b31cd84844c009ee9
uu <79706975+ImJustNon@users.noreply.github.com>
2026-06-26 04:21:55 +0700

    feat: utils code for get StudentId from access token

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from "./app.service";
 import { PrismaModule } from "./core/prisma/prisma.module";
 import { AuthModule as BetterAuthModule } from "@thallesp/nestjs-better-auth";
 import { auth } from "@repo/auth/server";
+import { UtilsModule } from "./modules/utils/utils.module";
 
 @Module({
-	imports: [PrismaModule, BetterAuthModule.forRoot({ auth })],
+	imports: [PrismaModule, BetterAuthModule.forRoot({ auth }), UtilsModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
