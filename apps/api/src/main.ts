4cbaaa794bd35be7319b64fc100293be4b0c2644
uu <79706975+ImJustNon@users.noreply.github.com>
2026-06-26 01:49:18 +0700

    feat: init dev tool middleware

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,30 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { config } from "@repo/config";
+import { ValidationPipe } from "@nestjs/common";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
+		logger: ["log", "error", "warn", "debug", "verbose"],
 		bodyParser: false,
 	});
+
+	app.enableCors({
+		origin: config.backend.allowOrigins,
+		credentials: true,
+	});
+
+	app.useGlobalPipes(new ValidationPipe());
+	app.use(cookieParser());
+	app.use(cookieParser());
+	app.use(morgan("dev"));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.getHttpAdapter().getInstance().set("trust proxy", 1);
+
 	await app.listen(config.backend.port);
 }
 bootstrap();
