690f990af71fa9b3882f5fc40f8f027e4f6e2d8d
uu <79706975+ImJustNon@users.noreply.github.com>
2026-06-27 17:58:21 +0700

    refactor: remove build in entrypoint

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -e
 
+echo "Building Package"
+pnpm --filter @repo/config build
+pnpm --filter @repo/database build
+pnpm --filter @repo/auth build
+pnpm --filter @repo/storage build
+
 echo "Running Database Migrations..."
 pnpm --filter @repo/database run db:migrate:deploy
 
-echo "Generating Prisma Client..."
-pnpm --filter @repo/database run generate
+# echo "Generating Prisma Client..."
+# pnpm --filter @repo/database run generate
 
 echo "Starting API..."
 exec "$@"
