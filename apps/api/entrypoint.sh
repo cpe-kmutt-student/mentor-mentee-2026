ee4852df252cf980c1636c32b85b6384e25a4a37
uu <79706975+ImJustNon@users.noreply.github.com>
2026-06-27 18:50:32 +0700

    refactor: remove buid outside docker

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Building Package"
-pnpm --filter @repo/config build
-pnpm --filter @repo/database build
-pnpm --filter @repo/auth build
-pnpm --filter @repo/storage build
-
 echo "Running Database Migrations..."
 pnpm --filter @repo/database run db:migrate:deploy
 
