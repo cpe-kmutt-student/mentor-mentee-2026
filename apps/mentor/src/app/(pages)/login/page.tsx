dfe902ba6095632b5ad211e389cb3eded99ecfd5
uu <79706975+ImJustNon@users.noreply.github.com>
2026-06-28 19:29:35 +0700

    feat: setup next for mentor profile web

---
+++ apps/mentor/src/app/(pages)/login/page.tsx
@@ -0,0 +1,14 @@
+import type React from "react";
+
+export default function LoginPage(): React.JSX.Element {
+	return (
+		<>
+			<div className="container bg-amber-700 w-full mx-auto">
+				<div className="bg-red-500  text-black w-full text-center">asd</div>
+			</div>
+			<button type="button" className="bg-blue-500">
+				Button A
+			</button>
+		</>
+	);
+}
